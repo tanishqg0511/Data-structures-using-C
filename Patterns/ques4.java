1acebb03e221e90bd65e427693ba63db2572e527
tanishqg0511 <91174393+tanishqg0511@users.noreply.github.com>
2022-09-08 19:32:30 +0530

    Create ques4.java

---
+++ Patterns/ques4.java
@@ -0,0 +1,24 @@
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+import java.util.*;
+public class Main
+{
+    public static void pattern(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<i+1;j++)
+            System.out.print(i);
+            System.out.println("");
+        }
+    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		pattern(n);
+	}
+}
