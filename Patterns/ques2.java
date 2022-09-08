888352a998bb5f510d7dfc572f105b5783a1e285
tanishqg0511 <91174393+tanishqg0511@users.noreply.github.com>
2022-09-08 19:25:52 +0530

    Create ques2.java

---
+++ Patterns/ques2.java
@@ -0,0 +1,24 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+import java.util.*;
+public class Main
+{
+    public static void pattern(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i+1;j++)
+            System.out.print("*");
+            System.out.println("");
+        }
+    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		pattern(n);
+	}
+}
